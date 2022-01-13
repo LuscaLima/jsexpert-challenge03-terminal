d99e567828c022dea3ef4d5aba17837de92c76b7
Lusca Lima <lusca.lima@outlook.com>
2022-01-12 23:35:11 -0300

    feat: implementa o `IncomeService`

--- src/service/IncomeService.js
+++ src/service/IncomeService.js
@@ -1,16 +1,62 @@
-import IncomeRepository from './../repository/IncomeRepository.js';
-import Income from './../entity/Income.js';
+import IncomeRepository from "./../repository/IncomeRepository.js";
+import Income from "./../entity/Income.js";
+import language from "../config/language.js";
+
+const defaultLanguage = language.default;
 
 class IncomeService {
-  constructor({ incomeRepository } = {}) {
-    this.incomeRepository = incomeRepository || new IncomeRepository();
+  constructor({ incomeRepository = new IncomeRepository() } = {}) {
+    this.incomeRepository = incomeRepository;
   }
 
-  async generateIncomeFromString(incomeString, delimiter = ';') {
-    const [position, expectation] = incomeString.split(delimiter);
+  async generateIncomeFromString(incomeString, delimiter = ";") {
+    const [position, rawExpectationValue] = incomeString.split(delimiter);
 
-    // @TODO: Implement method
-    return null;
+    if (!position) {
+      throw new Error(
+        "Position is a required field. Please make sure you are providing a position."
+      );
+    }
+    if (/\D/g.test(String(rawExpectationValue).trim())) {
+      throw new Error(
+        "A valid Expectation is required. Please note that only numbers are allowed."
+      );
+    }
+
+    const expectationValue = Number(rawExpectationValue);
+    const { USD, EUR, RUB } = await this.incomeRepository.getConversions();
+
+    const expectation = {
+      language: defaultLanguage,
+      value: expectationValue,
+      currency: "BRL",
+    };
+
+    const conversion01 = {
+      language: "en-US",
+      value: expectationValue * USD,
+      currency: "USD",
+    };
+
+    const conversion02 = {
+      language: "en-GB",
+      value: expectationValue * EUR,
+      currency: "EUR",
+    };
+
+    const conversion03 = {
+      language: "ru-RU",
+      value: expectationValue * RUB,
+      currency: "RUB",
+    };
+
+    return new Income({
+      position,
+      expectation,
+      conversion01,
+      conversion02,
+      conversion03,
+    });
   }
 }
 
